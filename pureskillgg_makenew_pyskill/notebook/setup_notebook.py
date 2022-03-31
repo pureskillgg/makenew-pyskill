4c83b0db9903b12d0a5f43666e2a028843528970
Bill Freeman <bill@pureskill.gg>
2022-03-31 12:12:17 -0500

    Linter fixes

--- pureskillgg_makenew_pyskill/notebook/setup_notebook.py
+++ pureskillgg_makenew_pyskill/notebook/setup_notebook.py
@@ -6,7 +6,7 @@ from ..env_functions.env_setup import setup_env_from_dotenv, echo_paths
 
 
 def setup_notebook(silent=False):
-    ''' Setup notebook environment '''
+    """Setup notebook environment"""
     setup_env_from_dotenv()
     if not silent:
         echo_paths()
